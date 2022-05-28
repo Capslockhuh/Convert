27a6f6456ee684075ce97a953d95cc756965e9bd
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-05-28 10:19:08 +0200

    added a unit picker

--- Convert/ContentView.swift
+++ Convert/ContentView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let conversionUnits = ["Celcius to Farenheit", "Liters to Pints", "Meters to Feet"]
+    
     @State private var userInput: Double = 0
+    @State private var conversionUnit = "Celcius to Farenheit"
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Enter your unit here", value: $userInput, format: .number)
                 }
+                
+                Section {
+                    Picker("Unit Conversion", selection: $conversionUnit) {
+                        ForEach(0..<3) {
+                            Text(conversionUnits[$0])
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("Placeholder", format: .number)
+                } header: {
+                     Text("Converted unit")
+                }
             }
+            .navigationTitle("Convert")
         }
-        .navigationTitle("Convert")
     }
 }
 

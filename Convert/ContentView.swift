6526e8777fe510b08525aa8bb404832f66b074a3
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-05-28 09:56:57 +0200

    Initial Commit

---
+++ Convert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Convert
+//
+//  Created by Jan Andrzejewski on 28/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Jan Andrzejewski on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
